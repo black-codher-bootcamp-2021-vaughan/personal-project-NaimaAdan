88c0aa2b0c1ba3645031fc720e3c119b46a41f7d
Suaad <suaadsaeed111@gmail.com>
2021-04-07 16:31:02 +0100

    labels added to queue

--- src/queue.js
+++ src/queue.js
@@ -17,13 +17,23 @@ function dequeueArray(jsav, jsavArray, value) {
 
 function run() {
     var jsav = new JSAV("container");
+     let headPointer;
+     let tailPointer;
           var myArr = []
           jsavArr = jsav.ds.array(myArr, {layout: "bar"});
           var pushElement = document.getElementById("enqueue");
           pushElement.addEventListener("click", function() {
-              var input = document.getElementById("userInput")
-              var valueToAdd = input.value
-              jsavArr = enqueueArray(jsav, jsavArr, valueToAdd) 
+        var input = document.getElementById("userInput")
+        var valueToAdd = input.value
+        jsavArr = enqueueArray(jsav, jsavArr, valueToAdd) 
+        if (headPointer) {
+            headPointer.hide()
+        }
+        if (tailPointer) {
+            tailPointer.hide()
+          }
+          tailPointer = jsav.pointer("tail", jsavArr.index(jsavArr.size() - 1), {arrowAnchor: "left top"})
+          headPointer = jsav.pointer("Head", jsavArr.index(0), {arrowAnchor: "left top"})
       })
 
       var newArr = []
@@ -33,6 +43,14 @@ function run() {
               var input = document.getElementById("userInput")
               var valueToRemove = input.value
               jsavArr = dequeueArray(jsav, jsavArr, valueToRemove) 
+              if (headPointer) {
+                headPointer.hide()
+            }
+            if (tailPointer) {
+                tailPointer.hide()
+              }
+              tailPointer = jsav.pointer("tail", jsavArr.index(jsavArr.size() - 1), {arrowAnchor: "left top"})
+              headPointer = jsav.pointer("Head", jsavArr.index(0), {arrowAnchor: "left top"})
       })
 
 
