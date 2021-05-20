c982158c8b8755a00b1e1b0cd9e37c2bf1053e58
suaadMohamed <64432679+suaadMohamed@users.noreply.github.com>
2021-05-20 21:05:39 +0100

    Cleanup files (#1)

--- src/front.js
+++
@@ -1,6 +0,0 @@
-/*!
- * JSAV - JavaScript Algorithm Visualization Library
- * Version v1.0.1-33-g556853c
- * Copyright (c) 2011-2015 by Ville Karavirta and Cliff Shaffer
- * Released under the MIT license.
- */
